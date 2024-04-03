331916de013b8d32f3a44e3eae80b9030a562af4
AnaVucic <anavucic007@gmail.com>
2024-04-03 14:07:45 +0200

    initial commit

---
+++ src/main/java/com/napredno/doggroom/DoggroomApplication.java
@@ -0,0 +1,13 @@
+package com.napredno.doggroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoggroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoggroomApplication.class, args);
+	}
+
+}
