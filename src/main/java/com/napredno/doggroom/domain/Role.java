1db9c128a424c10c65554e0a3b9bd5fb77f0744a
AnaVucic <anavucic007@gmail.com>
2024-04-29 03:53:15 +0200

    changes to domain classes for role and user

--- src/main/java/com/napredno/doggroom/domain/Role.java
+++ src/main/java/com/napredno/doggroom/domain/Role.java
@@ -1,28 +1,6 @@
 package com.napredno.doggroom.domain;
 
-import jakarta.persistence.*;
-
-@Entity
-@Table(name = "roles")
-public class Role {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+public enum Role {
+    USER,
+    ADMIN
 }
